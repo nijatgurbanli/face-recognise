999a5908a6392a8a6f44cf53774a4a5025db13ca
Nijat <44805343+nijatgurbanli@users.noreply.github.com>
2023-06-13 10:26:57 +0400

    nijatgurbanli

---
+++ src/FetchData.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// // import './App.css';
+//
+// import React, {useEffect, useState} from 'react';
+// const FetchData
+//     return
+// }
+//
+// export default FetchData;
